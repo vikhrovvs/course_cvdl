72c5413371dc0709490d157752ae67a90ecf66aa
Vladislav Vikhrov <vikhrov.vs@phystech.edu>
2022-10-10 23:13:25 +0300

    linear

--- task1/abbyy_course_cvdl_t1/linear.py
+++ task1/abbyy_course_cvdl_t1/linear.py
@@ -13,10 +13,25 @@ class LinearLayer(BaseLayer):
     """
     def __init__(self, in_features: int, out_features: int):
         super().__init__()
-        raise NotImplementedError()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        self.parameters.append(np.random.randn(out_features, in_features))
+        self.parameters.append(np.random.randn(out_features))
+
+        self.parameters_grads.append(np.zeros((out_features, in_features)))
+        self.parameters_grads.append(np.zeros(out_features))
+
+        self.input = None
 
     def forward(self, input: np.ndarray) -> np.ndarray:
-        raise NotImplementedError()
+        self.input = input
+        return input @ self.parameters[0].T + self.parameters[1]
 
     def backward(self, output_grad: np.ndarray) -> np.ndarray:
-        raise NotImplementedError()
+        self.parameters_grads[0] += np.sum(
+            (output_grad.swapaxes(-1, -2) @ self.input).reshape(-1, self.input.shape[-1] * output_grad.shape[-1]),
+            axis=0) \
+            .reshape(self.parameters_grads[0].shape)
+        self.parameters_grads[1] += np.sum(output_grad.reshape(-1, output_grad.shape[-1]), axis=0)
+        return output_grad @ self.parameters[0]
